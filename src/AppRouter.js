aa149c334b9f402ddd3eaa0c226517c554389e44
jack <mengbinhao2018@gmail.com>
2019-09-10 18:01:41 +0800

    first comment

---
+++ src/AppRouter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AppRouter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div>
+        jack
+      </div>
+     )
+  }
+}
+
+export default AppRouter
